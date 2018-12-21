f2257fad1252dd8374dd921b317ac6c0f5199efe
Govern <745087337@qq.com>
2018-12-21 17:43:51 +0800

    "AM Dynamic"

--- AM.py
+++ AM.py
@@ -5,17 +5,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#定义参数
-k = 0.99             #调幅深度
-Ucm = 1             #直流分量
-Um = 1              #载波分量
-W  = 2*np.pi*2      #基波频率
-Wc = 2*np.pi*20    #载波频率
+plt.ion()
+for  i in range(100):
+    #定义参数
+    k = i/100.          #调制深度
+    Ucm = 1             #直流分量
+    Um = 1              #载波分量
+    W  = 2*np.pi*2      #基波频率
+    Wc = 2*np.pi*20    #载波频率
 
-t = np.linspace(-3,3,5000)
-y = (Ucm+k*Um*np.cos(W*t))*np.cos(Wc*t)
+    t = np.linspace(-3,3,5000)
+    y = (Ucm+k*Um*np.cos(W*t))*np.cos(Wc*t)
 
-plt.plot(t,y,linestyle='--',color='r')
-plt.xlabel(r'Time/s')
-plt.ylabel(r'Amplitude/V')
+    plt.cla()
+    plt.plot(t,y,linestyle='--',color='r')
+    plt.xlabel(r'Time/s')
+    plt.ylabel(r'Amplitude/V')
+    plt.show()
+    plt.pause(0.1)
+
+plt.ioff()
 plt.show()
